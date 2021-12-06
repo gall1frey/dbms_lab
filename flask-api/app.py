c043589d3671e1833f55f0349068406a88e53183
Gallifrey <sird.mallika@gmail.com>
2021-12-06 11:51:42 +0530

    Added all functionality

--- flask-api/app.py
+++ flask-api/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 import psycopg2 as pg2
 import psycopg2.extras
+from datetime import date
 
 app = Flask(__name__)
 CORS(app)
@@ -16,6 +17,10 @@ except:
 @app.route('/',methods=['GET'])
 def home():
 	return render_template('index.html')
+
+@app.route('/reception',methods=['GET'])
+def reception():
+	return render_template('reception.html')
 
 @app.route('/accounts',methods=['GET'])
 def accounts():
@@ -85,6 +90,15 @@ def rec_pay_bill():
 	print(request.form)
 	#return {'msg':'test'}
 	cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+	query = "SELECT * FROM bill WHERE b_paymode IS NOT NULL AND b_id={} AND cust_id={}"
+	q = query.format(request.form['customer_id'],
+					request.form['bill_id'])
+	cursor.execute(q)
+	count = 0
+	for _ in cursor:
+		count += 1
+	if count == 0:
+		return {'msg':'No such bill due!'}
 	query = "UPDATE BILL SET B_PAYMODE ='{}' WHERE CUST_ID={} AND B_ID = {}"
 	q = query.format(request.form['paymode'],
 					request.form['customer_id'],
@@ -97,7 +111,7 @@ def rec_pay_bill():
 @app.route('/reception/get_bill',methods=['POST'])
 def get_bill():
 	cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-	query = "SELECT B_INFO,B_AMT,B_PAYMODE,B_DATE FROM BILL,CUSTOMER WHERE BILL.CUST_ID=C_ID AND CUSTOMER.C_LNAME='{}' AND C_PHNO={}"
+	query = "SELECT B_ID,B_INFO,B_AMT,B_PAYMODE,B_DATE FROM BILL,CUSTOMER WHERE BILL.CUST_ID=C_ID AND CUSTOMER.C_LNAME='{}' AND C_PHNO={}"
 	q = query.format(request.form['cust_lname'],
 					request.form['phno'])
 	cursor.execute(q)
@@ -107,6 +121,81 @@ def get_bill():
 	cursor.close()
 	return {'data':l}
 
-@app.route('/reception',methods=['GET'])
-def reception():
-	return render_template('reception.html')
+@app.route('/accounts/list',methods=['POST'])
+def list_accounts():
+	symbols = {'gt':'>','lt':'<','eq':'='}
+	cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+	query = "SELECT {} FROM EMPLOYEE WHERE {}"
+	f = dict(request.form)
+	cols = [f[i] for i in f if i[0] != 'c']
+	if len(cols) == 0:
+		return {'msg':'No Columns selected!'}
+	cond = request.form['condition'] + symbols[request.form['comparison']]
+	if request.form['check'].isdigit():
+		cond += request.form['check']
+	else:
+		cond += '"{}"'.format(request.form['check'])
+	q = query.format(','.join(cols),
+					cond)
+	cursor.execute(q)
+	l = []
+	for i in cursor:
+		l.append(i)
+	cursor.close()
+	return {'data':l,'cols':cols}
+
+@app.route('/accounts/update',methods=['POST'])
+def update_accounts():
+	return {'msg':'fend working'}
+
+@app.route('/services/list',methods=['POST'])
+def list_services():
+	cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+	query = "SELECT s_id,s_name,s_description,s_charges FROM services WHERE dept_id={}"
+	print(request.form)
+	q = query.format(request.form['dept'])
+	cursor.execute(q)
+	l = []
+	for i in cursor:
+		l.append(i)
+	cursor.close()
+	return {'data':l}
+
+@app.route('/services/use',methods=['POST'])
+def use_services():
+	print(request.form)
+	#return {'msg':'test'}
+	today = date.today()
+	dt = today.strftime("%m/%d/%y")
+	cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+	cursor.execute('SELECT MAX(B_ID) FROM bill;')
+	max = 0
+	for i in cursor:
+		max=i
+	max = max[0]
+	max += 1
+	query = "SELECT s_name,s_charges FROM services WHERE s_id={}"
+	q = query.format(request.form['service_id'])
+	cursor.execute(q)
+	charges = -1
+	desc = ''
+	for i in cursor:
+		desc,charges = i
+	if len(desc) == 0 or charges < 0:
+		return {'msg':'Invalid Data'}
+	desc += ' x'+str(request.form['quantity'])
+	charges = float(charges)*float(request.form['quantity'])
+	query = "SELECT c_id FROM customer WHERE c_lname='{}' AND c_phno={} AND c_checked_in=true"
+	q = query.format(request.form['cust_lname'],request.form['cust_phno'])
+	cursor.execute(q)
+	id = -1
+	for i in cursor:
+		id = i[0]
+	if id < 0:
+		return {'msg':'No such customer!'}
+	query = "INSERT INTO bill (b_info,b_amt,b_id,b_date,cust_id) VALUES ('{}',{},{},'{}',{})"
+	q = query.format(desc,charges,max,dt,id)
+	cursor.execute(q)
+	conn.commit()
+	cursor.close()
+	return {'msg':'Service added!'}
--- flask-api/static/script.js
+++ flask-api/static/script.js
@@ -38,7 +38,7 @@ $('#bill_sub').click(function(event) {
 						timeout: 800000,
 						success: function (data) {
 							if(data.hasOwnProperty('msg')) {
-								$('#msg').innerHTML = data['msg'];
+								document.getElementById('msg').innerHTML = data['msg'];
 							}
 						},
 						error: function (e) {
@@ -83,41 +83,181 @@ $('#checkout_form_sub').click(function(event) {
 });
 
 $('#get_bill_sub').click(function(event) {
-		event.preventDefault();
-		var form = $('#get_bill_form')[0];
-		var data = new FormData(form);
-		$.ajax({
-						type: "POST",
-						enctype: 'multipart/form-data',
-						url: "/reception/get_bill",
-						data: data,
-						processData: false,
-						contentType: false,
-						cache: false,
-						timeout: 800000,
-						success: function (data) {
-								table_text = `<tr><th>INFO</th><th>AMOUNT</th>
-															<th>PAYMENT MODE</th><th>DATE</th></tr>`;
-								for(var i = 0; i < data.data.length; i++){
-									table_text += `<tr>`;
-									for(var j=0; j < data.data[i].length; j++){
-										table_text += `<td>`+data.data[i][j]+`</td>`;
-									}
-									table_text += `</tr>`;
-									//console.log(data.data[i]);
+	event.preventDefault();
+	var form = $('#get_bill_form')[0];
+	var data = new FormData(form);
+	$.ajax({
+					type: "POST",
+					enctype: 'multipart/form-data',
+					url: "/reception/get_bill",
+					data: data,
+					processData: false,
+					contentType: false,
+					cache: false,
+					timeout: 800000,
+					success: function (data) {
+							table_text = `<tr><th>BILL ID</th><th>INFO</th><th>AMOUNT</th>
+														<th>PAYMENT MODE</th><th>DATE</th></tr>`;
+							for(var i = 0; i < data.data.length; i++){
+								table_text += `<tr>`;
+								for(var j=0; j < data.data[i].length; j++){
+									table_text += `<td>`+data.data[i][j]+`</td>`;
 								}
-								document.getElementById('reception_table').innerHTML = table_text;
-						},
-						error: function (e) {
-								console.log("ERROR : ", e);
-						}
-				});
+								table_text += `</tr>`;
+								//console.log(data.data[i]);
+							}
+							document.getElementById('reception_table').innerHTML = table_text;
+					},
+					error: function (e) {
+							console.log("ERROR : ", e);
+					}
+			});
 });
 
-$('#form_selector').change(function(event) {
-	var form = '#'+document.getElementById('form_selector').value;
+$('#list_accounts_sub').click(function(event) {
+	event.preventDefault();
+	var form = $('#accounts_list_form')[0];
+	var data = new FormData(form);
+	$.ajax({
+					type: "POST",
+					enctype: 'multipart/form-data',
+					url: "/accounts/list",
+					data: data,
+					processData: false,
+					contentType: false,
+					cache: false,
+					timeout: 800000,
+					success: function (data) {
+							console.log(data);
+							table_text = `<tr>`;
+							for(var i = 0; i < data.cols.length; i++){
+								table_text += `<th>`+data.cols[i]+`</th>`;
+							}
+							table_text += `<tr>`;
+							for(var i = 0; i < data.data.length; i++){
+								table_text += `<tr>`;
+								for(var j=0; j < data.data[i].length; j++){
+									table_text += `<td>`+data.data[i][j]+`</td>`;
+								}
+								table_text += `</tr>`;
+								//console.log(data.data[i]);
+							}
+							document.getElementById('accounts_table').innerHTML = table_text;
+					},
+					error: function (e) {
+							console.log("ERROR : ", e);
+					}
+			});
+});
+
+$('#list_serv_sub').click(function(event) {
+	event.preventDefault();
+	var form = $('#list_services_form')[0];
+	var data = new FormData(form);
+	$.ajax({
+					type: "POST",
+					enctype: 'multipart/form-data',
+					url: "/services/list",
+					data: data,
+					processData: false,
+					contentType: false,
+					cache: false,
+					timeout: 800000,
+					success: function (data) {
+							//console.log(data);
+							table_text = `<tr><th>SERVICE ID</th><th>SERVICE NAME</th>
+														<th>SERVICE DESCRIPTION</th><th>SERVICE CHARGES</th></tr>`;
+							for(var i = 0; i < data.data.length; i++){
+								table_text += `<tr>`;
+								for(var j=0; j < data.data[i].length; j++){
+									table_text += `<td>`+data.data[i][j]+`</td>`;
+								}
+								table_text += `</tr>`;
+								//console.log(data.data[i]);
+							}
+							document.getElementById('service_table').innerHTML = table_text;
+					},
+					error: function (e) {
+							console.log("ERROR : ", e);
+					}
+			});
+});
+
+$('#use_serv_sub').click(function(event) {
+	event.preventDefault();
+	var form = $('#use_services_form')[0];
+	var data = new FormData(form);
+	$.ajax({
+					type: "POST",
+					enctype: 'multipart/form-data',
+					url: "/services/use",
+					data: data,
+					processData: false,
+					contentType: false,
+					cache: false,
+					timeout: 800000,
+					success: function (data) {
+						document.getElementById('msg').innerHTML = data.msg;
+					},
+					error: function (e) {
+							console.log("ERROR : ", e);
+					}
+			});
+});
+
+$('#update_accounts_sub').click(function(event) {
+	event.preventDefault();
+	var form = $('#accounts_update_form')[0];
+	var data = new FormData(form);
+	$.ajax({
+					type: "POST",
+					enctype: 'multipart/form-data',
+					url: "/accounts/update",
+					data: data,
+					processData: false,
+					contentType: false,
+					cache: false,
+					timeout: 800000,
+					success: function (data) {
+							console.log(data);
+							table_text = `<tr><th>INFO</th><th>AMOUNT</th>
+														<th>PAYMENT MODE</th><th>DATE</th></tr>`;
+							for(var i = 0; i < data.data.length; i++){
+								table_text += `<tr>`;
+								for(var j=0; j < data.data[i].length; j++){
+									table_text += `<td>`+data.data[i][j]+`</td>`;
+								}
+								table_text += `</tr>`;
+								//console.log(data.data[i]);
+							}
+							document.getElementById('accounts_table').innerHTML = table_text;
+					},
+					error: function (e) {
+							console.log("ERROR : ", e);
+					}
+			});
+});
+
+$('#form_selector_reception').change(function(event) {
+	var form = '#'+document.getElementById('form_selector_reception').value;
 	$('.reception-form').css({'display':'none'});
 	$(form).css({'display':'block'});
-	document.getElementById('reception_table').innerHTML = '';
+	//document.getElementById('reception_table').innerHTML = '';
+	document.getElementById('msg').innerHTML = '';
+});
+
+$('#form_selector_services').change(function(event) {
+	var form = '#'+document.getElementById('form_selector_services').value;
+	$('.service-form').css({'display':'none'});
+	$(form).css({'display':'block'});
+	//document.getElementById('service_table').innerHTML = '';
+	//document.getElementById('msg').innerHTML = '';
+});
+
+$('#form_selector_accounts').change(function(event) {
+	var form = '#'+document.getElementById('form_selector_accounts').value;
+	$('.accounts-form').css({'display':'none'});
+	$(form).css({'display':'block'});
+	document.getElementById('accounts_table').innerHTML = '';
 	document.getElementById('msg').innerHTML = '';
 });
